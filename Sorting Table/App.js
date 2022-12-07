a243ac1061db3dfb4443e5156ba780abdeb5f3ee
1992aruna <113091758+1992aruna@users.noreply.github.com>
2022-12-07 03:21:58 -0800

    Add files via upload

---
+++ Sorting Table/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { SortingTable } from './components/SortingTable'
+
+function App() {
+  return (
+    <div className='App'>
+      <SortingTable />
+    </div>
+  )
+}
+
+export default App
